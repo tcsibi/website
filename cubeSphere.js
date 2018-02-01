48d448f21b4a5a033daf3ba3efd25e6700b43353
tcsibi <35610448+tcsibi@users.noreply.github.com>
2018-01-31 20:49:40 -0500

    Update cubeSphere.js

--- cubeSphere.js
+++ cubeSphere.js
@@ -1,40 +1,6 @@
 
 
-		<script type="x-shader/x-vertex" id="vertexShader">
-
-			attribute vec3 center;
-			varying vec3 vCenter;
-
-			void main() {
-
-				vCenter = center;
-				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-			}
-
-		</script>
-
-		<script type="x-shader/x-fragment" id="fragmentShader">
-
-			varying vec3 vCenter;
-
-			float edgeFactorTri() {
-
-				vec3 d = fwidth( vCenter.xyz );
-				vec3 a3 = smoothstep( vec3( 0.0 ), d * 1.5, vCenter.xyz );
-				return min( min( a3.x, a3.y ), a3.z );
-
-			}
-
-			void main() {
-
-				gl_FragColor.rgb = mix( vec3( 1.0 ), vec3( 0.2 ), edgeFactorTri() );
-				gl_FragColor.a = 1.0;
-			}
-
-		</script>
-
-		<script>
+		
 
 			var cs_camera, cs_scene, cs_renderer;
 
@@ -128,5 +94,5 @@
 
 			}
 
-		</script>
+		
 
